551356cccc96ef2a11622a89fd70a694ae156fd5
Enhu <eze.eki@gmail.com>
2019-03-17 18:32:24 -0300

    Added self assing role functionality

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 //bot variable declarations
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const discordClient = new Discord.Client();
 const config = require("./config.json");
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: config.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+const query = {
+  text: 'SELECT * FROM roles;',
+}
+
+function loadRoles(){
+  client.query(query)
+  .then(res => {
+    roles = res.rows;
+  }).catch(e => {
+    console.error(e.stack)
+  })
+}
+
+loadRoles();
 
 let prefix = config.prefix;
 var roles = [];
+
 //connects the bot to the discord users
-client.login(config.token);
+discordClient.login(config.token);
 
 //bot logged in successfully and it's ready to be used
-client.on("ready", () => {
-  console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+discordClient.on("ready", () => {
+  console.log(`Ready to server in ${discordClient.channels.size} channels on ${discordClient.guilds.size} servers, for a total of ${discordClient.users.size} users.`);
 });
 
-client.on("message", (message) => {
+discordClient.on("message", (message) => {
 
   //any message done by the bot will return nothing
   if(message.author.bot) return;
@@ -23,67 +47,141 @@ client.on("message", (message) => {
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+  const substr = message.content.substr((prefix + command + " ").length);
 
+  //adds a role to the list so user can self assign them
   if(command === "setrole"){
-    if(message.guild.roles.find(role => role.name === args[0])){
-        if(checkForRole(roles, args[0])){
-            message.channel.send("Role " + args[0] + " has already been added to the list.")
+    if(substr != ''){
+      const query = {
+        text: 'SELECT * FROM roles;',
+      }
+      // promise
+      client.query(query)
+        .then(res => {
+          roles = res.rows;
+          serverID = message.guild.id;
+          if(message.guild.roles.find(role => role.name === substr)){
+            if(roles.some(item => item.rolename === substr) && roles.some(item => item.serverid === serverID)){
+                message.channel.send("Role ``" + substr + "`` has already been added to the list.")
+            }else{
+              const text = 'INSERT INTO roles(roleName, serverID) VALUES($1, $2)'
+              const values = [substr, serverID]
+              
+              // promise
+              client.query(text, values)
+                .then(res => {
+                  message.channel.send("Role ``" + substr + "`` successfully added to the list.")
+                  loadRoles();
+                })
+                .catch(e => {
+                  console.error(e.stack)
+                })
+            } 
         }else{
-            roles.push(args[0]);
-            message.channel.send("Role " + args[0] + " successfully added to the list.")
-        } 
+            message.channel.send("I didn't find any role called ``" + substr + "`` on this server.")
+        }
+        })
+        .catch(e => {
+          console.error(e.stack)
+        })
     }else{
-        message.channel.send("I didn't find any role called " + args[0] + " on this server.")
+      message.channel.send("``!setrole [role]``"); 
+    }
+  }
+
+  if(command === "removerole"){
+    if(substr != ''){
+      const query = {
+        text: 'SELECT * FROM roles;',
+      }
+      // promise
+      client.query(query)
+        .then(res => {
+          roles = res.rows;
+          serverID = message.guild.id;
+          if(message.guild.roles.find(role => role.name === substr)){
+            if(roles.some(item => item.rolename === substr) && roles.some(item => item.serverid === serverID)){
+              const text = "DELETE FROM roles WHERE roleName='" + substr +"'";
+              const values = [substr];
+              
+              // promise
+              client.query(text)
+                .then(res => {
+                  message.channel.send("Role ``" + substr + "`` successfully deleted from the list.")
+                  loadRoles();
+                })
+                .catch(e => {
+                  console.error(e.stack)
+                })
+                
+            }else{
+              message.channel.send("Role ``" + substr + "`` doesn't exist on the list. Try adding it with !setrole [role].")
+            } 
+        }else{
+            message.channel.send("I didn't find any role called ``" + substr + "`` on this server.")
+        }
+        })
+        .catch(e => {
+          console.error(e.stack)
+        })
+    }else{
+      message.channel.send("``!removerole [role]``"); 
     }
   }
 
   //gives the user a role if they didn't have it and the role is on the list
-  if(command === "getrole"){
-    let member = message.member;
-    if (message.member.roles.find(role => role.name === args[0])){
-      message.channel.send("You already have the " + args[0] + " role.");
+  if(command === "iam"){
+    if(substr != ''){
+      let member = message.member;
+      if (message.member.roles.find(role => role.name === substr)){
+        message.channel.send("You already have the ``" + substr + "`` role.");
+      }else{
+          if(roles.some(item => item.rolename === substr)){ //
+              let role = message.guild.roles.find(role => role.name === substr);
+              member.addRole(role).catch(console.error);
+              message.react('☑').then(console.log).catch(console.error);
+          }else{
+              if(message.guild.roles.find(role => role.name === substr)){
+                  message.channel.send("I don't have permissions to give this role.");
+              }else{
+                  message.channel.send("I didn't find any role called ``" + substr + "`` on this server.");
+              }
+          }
+      }
     }else{
-        if(checkForRole(roles, args[0])){
-            let role = message.guild.roles.find(role => role.name === args[0]);
-            member.addRole(role).catch(console.error);
-            message.react(message.guild.emojis.get('123456789012345678')).then(console.log).catch(console.error);
-        }else{
-            if(message.guild.roles.find(role => role.name === args[0])){
-                message.channel.send("I don't have permissions to give this role.");
-            }else{
-                message.channel.send("I didn't find any role called " + args[0] + " on this server.");
-            }
-        }
+      message.channel.send("!iam [role]"); 
+      /*for (let name of roles.rolename) {
+        message.channel.send(JSON.stringify(name));
+      }*/
     }
   }
 
   //removes the role from a user if they had it and it exists on the list
-  if(command === "removerole"){
-    const guildMember = message.member;
-    if(checkForRole(roles, args[0])){
-        if (message.member.roles.find(role => role.name === args[0])){
-            let role = message.guild.roles.find(role => role.name === args[0]);
-            guildMember.removeRole(role).catch(console.error);
-            message.react(message.guild.emojis.get('123456789012345678')).then(console.log).catch(console.error);
+  if(command === "iamn"){
+    if(substr != ''){
+      const guildMember = message.member;
+      if(roles.some(item => item.rolename === substr)){ //
+          if (message.member.roles.find(role => role.name === substr)){
+              let role = message.guild.roles.find(role => role.name === substr);
+              guildMember.removeRole(role).catch(console.error);
+              message.react('☑').then(console.log).catch(console.error);
+            }else{
+              message.channel.send("You don't have the ``" + substr + "`` role.");
+            }
+      }else{
+          if(message.guild.roles.find(role => role.name === substr)){
+              message.channel.send("I don't have permissions to remove this role.");
           }else{
-            message.channel.send("You don't have the " + args[0] + " role.");
+              message.channel.send("I didn't find any role called ``" + substr + "`` on this server.");
           }
+      }
     }else{
-        if(message.guild.roles.find(role => role.name === args[0])){
-            message.channel.send("I don't have permissions to remove this role.");
-        }else{
-            message.channel.send("I didn't find any role called " + args[0] + " on this server.");
-        }
+      message.channel.send("``!iamn [role]``"); 
     }
   }
-
-  function checkForRole(array, val) {
-    return array.some(arrVal => val === arrVal);
-  }
-
 });
 
 //shows errors on console
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e));
-client.on("debug", (e) => console.info(e));
+discordClient.on("error", (e) => console.error(e));
+discordClient.on("warn", (e) => console.warn(e));
+discordClient.on("debug", (e) => console.info(e));
