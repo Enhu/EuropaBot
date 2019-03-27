c4f1ae8f61f6e7a1b6494d6815e649235b9ceac5
Enhu <eze.eki@gmail.com>
2019-03-26 22:08:17 -0300

    Fixed commands, added help command

--- index.js
+++ index.js
@@ -36,10 +36,13 @@ discordClient.on("ready", () => {
   console.log(`Ready to server in ${discordClient.channels.size} channels on ${discordClient.guilds.size} servers, for a total of ${discordClient.users.size} users.`);
 });
 
-discordClient.on("message", (message) => {
+discordClient.on("message", async (message) => {
 
   //any message done by the bot will return nothing
   if(message.author.bot) return;
+
+  //will only respond if talking in a discord server
+  if (!message.guild) return;
   
   //for performance purposes
   if (!message.content.startsWith(prefix)) return;
@@ -48,13 +51,16 @@ discordClient.on("message", (message) => {
   const command = args.shift().toLowerCase();
   const substr = message.content.substr((prefix + command + " ").length);
 
+//
+// SELF ASSIGNABLE ROLES
+//
+
   //adds a role to the list so user can self assign them
   if(command === "setrole"){
     if(substr != ''){
       const query = {
         text: 'SELECT * FROM roles;',
       }
-      // promise
       client.query(query)
         .then(res => {
           roles = res.rows;
@@ -65,8 +71,6 @@ discordClient.on("message", (message) => {
             }else{
               const text = 'INSERT INTO roles(roleName, serverID) VALUES($1, $2)'
               const values = [substr, serverID]
-              
-              // promise
               client.query(text, values)
                 .then(res => {
                   message.channel.send("Role ``" + substr + "`` successfully added to the list.")
@@ -77,14 +81,14 @@ discordClient.on("message", (message) => {
                 })
             } 
         }else{
-            message.channel.send("I didn't find any role called ``" + substr + "`` on this server.")
+            message.channel.send("I didn't find any role called ``" + substr + "`` on this server. Create the role on the server before using this command.")
         }
         })
         .catch(e => {
           console.error(e.stack)
         })
     }else{
-      message.channel.send("``!setrole [role]``"); 
+      message.channel.send("```!setrole [role]``` \nAdds roles to the self-assignable roles list."); 
     }
   }
 
@@ -93,7 +97,6 @@ discordClient.on("message", (message) => {
       const query = {
         text: 'SELECT * FROM roles;',
       }
-      // promise
       client.query(query)
         .then(res => {
           roles = res.rows;
@@ -102,11 +105,9 @@ discordClient.on("message", (message) => {
             if(roles.some(item => item.rolename === substr) && roles.some(item => item.serverid === serverID)){
               const text = "DELETE FROM roles WHERE roleName='" + substr +"'";
               const values = [substr];
-              
-              // promise
               client.query(text)
                 .then(res => {
-                  message.channel.send("Role ``" + substr + "`` successfully deleted from the list.")
+                  message.channel.send("Role ``" + substr + "`` successfully deleted from the self-assignable roles list.")
                   loadRoles();
                 })
                 .catch(e => {
@@ -114,17 +115,17 @@ discordClient.on("message", (message) => {
                 })
                 
             }else{
-              message.channel.send("Role ``" + substr + "`` doesn't exist on the list. Try adding it with !setrole [role].")
+              message.channel.send("Role ``" + substr + "`` doesn't exist on the list. Add it to the self-assignable roles with !setrole [rolename].")
             } 
         }else{
-            message.channel.send("I didn't find any role called ``" + substr + "`` on this server.")
+            message.channel.send("I didn't find any role called ``" + substr + "`` on this server. Create the role and then add it to the self-assignable roles with !setrole [rolename] before using this command.")
         }
         })
         .catch(e => {
           console.error(e.stack)
         })
     }else{
-      message.channel.send("``!removerole [role]``"); 
+      message.channel.send("```!removerole [role]``` \nRemoves roles from the self-assignable roles list."); 
     }
   }
 
@@ -143,15 +144,23 @@ discordClient.on("message", (message) => {
               if(message.guild.roles.find(role => role.name === substr)){
                   message.channel.send("I don't have permissions to give this role.");
               }else{
-                  message.channel.send("I didn't find any role called ``" + substr + "`` on this server.");
+                  message.channel.send("I didn't find any role called ``" + substr + "`` on this server. Create the role and then add it to the self-assignable roles with !setrole [rolename] before using this command.");
               }
           }
       }
     }else{
-      message.channel.send("!iam [role]"); 
-      /*for (let name of roles.rolename) {
-        message.channel.send(JSON.stringify(name));
-      }*/
+      let serverRoles = [];
+      for (let item of roles) {
+        if(message.guild.id == item.serverid){
+          serverRoles.push(item.rolename);
+        }
+      }
+      if(serverRoles.length > 0){
+        message.channel.send("```!iam [role] \nAdd yourself a role if available.\nAvailable roles are: " + serverRoles.toString() + "```"); 
+      }else{
+        message.channel.send("```!iam [role] \nAdd yourself a role if available.\nThere no available roles to add. Add roles to the list with !setrole [role]```");
+      }
+      
     }
   }
 
@@ -169,15 +178,38 @@ discordClient.on("message", (message) => {
             }
       }else{
           if(message.guild.roles.find(role => role.name === substr)){
-              message.channel.send("I don't have permissions to remove this role.");
+              message.channel.send("I don't have permissions to remove this role. Try adding it to the self-assignable roles list using !setrole [rolename]");
           }else{
-              message.channel.send("I didn't find any role called ``" + substr + "`` on this server.");
+            message.channel.send("I didn't find any role called ``" + substr + "`` on this server. Create the role and then add it to the self-assignable roles list with !setrole [rolename] before using this command.");
           }
       }
     }else{
-      message.channel.send("``!iamn [role]``"); 
+      let userRoles = [];
+      for (let item of roles) {
+        if(message.member.roles.find(role => role.name === item.rolename) && message.guild.id == item.serverid){
+          userRoles.push(item.rolename);
+        }
+      }
+      if(userRoles.length > 0){
+        message.channel.send("```!iamn [role] \nRemove yourself from a role.\nYour current roles are: " + userRoles.toString() + "```"); 
+      }else{
+        message.channel.send("```!iamn [role] \nRemove yourself from a role.\nYou don't have any roles I can remove.```");
+      }
+      
     }
   }
+
+  //direct messages a user for a list of commands
+  if(command === "help"){
+    message.author.send("```Available commands:\n\n!setrole [role]: Allows you to set an auto role.\n!removerole [role]: Allows you to remove an auto role.\n!iam [role]: Allows you to get a role.\n!iamn [role]: Allows you to remove a role.```");
+  }
+
+//
+//GBF RELATED CONTENT
+// to-do
+
+
+
 });
 
 //shows errors on console
