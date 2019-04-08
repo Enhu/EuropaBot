365f8bb6b1ab146e4be1fc947b381212693edb5f
Enhu <eze.eki@gmail.com>
2019-04-08 18:28:01 -0300

    Added database file, modified code

---
+++ _helpers/database.js
@@ -0,0 +1,99 @@
+const { Client } = require('pg');
+const config = require("../config.json");
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+
+client.connect();
+
+module.exports = {
+    selectAllDB : async function(table){
+        //connect to postgresql db
+        let results;
+        const query = "SELECT * FROM " + table + ";"
+        await client.query(query)
+        .then(res => {
+            results = res.rows;
+        }).catch(e => {
+            console.error(e.stack)
+        })
+        return results;
+    },
+
+    selectDB : async function(table, condition, value){
+        //connect to postgresql db
+        let results;
+        const query = "SELECT * FROM " + table + " WHERE " + condition + " = '" + value + "';"
+        await client.query(query)
+        .then(res => {
+            results = res.rows;
+        }).catch(e => {
+            console.error(e.stack)
+        })  
+
+        return results;
+    },
+    
+    updateDB : async function(table, userid, values){
+        var message;
+        let update = 'UPDATE ' + table + ' SET ';
+        let keys = Object.keys(values);
+        let set = [];
+        keys.forEach(function (key, i){
+            set.push(key + ' = ($' + (i + 1) + ')');
+        })
+        let queryValues = Object.keys(values).map(function (k) { return values[k]; });
+        const query = {
+            text: update + set.toString() + " WHERE userid = '" + userid + "';",
+            params: queryValues
+        }
+        await client.query(query.text, query.params)
+        .then(res => {
+            message = "OK";
+        })
+        .catch(e => {
+            console.error(e.stack)
+            message = e.stack;
+        })
+
+        return message;
+    },
+    
+    deleteDB : async function(table, condition, value){
+        let message;
+        const query = "DELETE FROM " + table + " WHERE " + condition + " = '" + value + "';"
+        await client.query(query)
+        .then(res => {
+            message = 'OK';
+        }).catch(e => {
+            console.error(e.stack)
+            message = e.stack;
+        })  
+
+        return message;
+    },
+    
+    insertDB : async function(table, values){
+        var message;
+        let insert = 'INSERT INTO ' + table;
+        let keys = Object.keys(values);
+        let dollar = keys.map(function (item, index) { return '$' + (index + 1); });
+        let queryValues = Object.keys(values).map(function (k) { return values[k]; });
+        const query = {
+            text: insert + '(' + keys + ')' + ' VALUES(' + dollar + ');',
+            params: queryValues
+        }
+        await client.query(query.text, query.params)
+        .then(res => {
+            message = "OK";
+        })
+        .catch(e => {
+            console.error(e.stack)
+            message = e.stack;
+        })
+
+        return message;
+    }
+}
